84de44a771aabb3133fb264eafc298695eca0c6a
BuildEnough <rhtmaehcl878@naver.com>
2024-07-08 14:47:07 +0900

    초기 설정

---
+++ board/src/main/java/com/project/board/MainController.java
@@ -0,0 +1,15 @@
+package com.project.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class MainController {
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+
+}
